4cbc9122c8333ae8d70b1d1feab269bab532fcb4
yudafu <yudafu@Richole.local>
2019-12-09 13:00:43 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const fs = require("fs")
+const https = require("https")
+const http = require("http")
+const path = require("path")
+const args = process.argv.slice(2)
+
+if (args.length === 2) {
+    const url = new URL(args[0])
+    const filename = args[1]
+    const isHttps = url.protocol === "https:"
+    const _http = isHttps ? https : http
+    const ws = fs.createWriteStream(path.resolve(process.cwd(), filename))
+    _http.get(url, res => res.pipe(ws))
+} else {
+    console.log("usage: download https://www.xxx.com/aaa.jpg a.jpg")
+}
